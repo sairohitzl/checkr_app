8341a62c18cd257f4d00f2a81d2ef1c8321d68ca
sairohitzl <sairohit.m@zemosolabs.com>
2023-02-07 11:48:48 +0530

    added jest

--- src/components/Atoms/Avatar/index.test.tsx
+++ src/components/Atoms/Avatar/index.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ProfileAvatar from ".";
+import React from "react";
 
 describe("avatar test", () => {
   it("avatar", () => {
